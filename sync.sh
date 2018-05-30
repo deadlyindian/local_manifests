c0aeabfd0a5e024dbb0dcbb13313c76c5e62237c
Akshay Menon <deadlyindian@users.noreply.github.com>
2018-05-30 05:54:39 +0000

    rebase: do it properly
    Signed-off-by: Akshay Menon <deadlyindian@users.noreply.github.com>

--- sync.sh
+++ sync.sh
@@ -27,58 +27,24 @@
   git pull $fab/packages_apps_Dialer oreo-mr1 --rebase;
   croot;
 
-  echo " ";
-  echo -e "\e[1;32mRebasing system/core...\e[0m";
-  cd system/core;
-  git pull $deadly$dcaf system_core --rebase;
-  croot;
+  dir2rebase=(
+    system/core
+    frameworks/av
+    system/media
+    system/sepolicy
+    vendor/du
+    packages/apps/Settings
+    build/make
+    hardware/libhardware
+    hardware/qcom/keymaster
+  )
 
-  echo " ";
-  echo -e "\e[1;32mRebasing frameworks/av...\e[0m";
-  cd frameworks/av;
-  git pull $deadly$dcaf frameworks_av --rebase;
-  croot;
-
-  echo " ";
-  echo -e "\e[1;32mRebasing system/media...\e[0m";
-  cd system/media;
-  git pull $deadly$dcaf system_media --rebase;
-  croot;
-
-  echo " ";
-  echo -e "\e[1;32mRebasing system/sepolicy...\e[0m";
-  cd system/sepolicy;
-  git pull $deadly$dcaf system_sepolicy --rebase;
-  croot;
-
-  echo " ";
-  echo -e "\e[1;32mRebasing vendor/du...\e[0m";
-  cd vendor/du;
-  git pull $deadly$dcaf vendor_du --rebase;
-  croot;
-
-  echo " ";
-  echo -e "\e[1;32mRebasing packages/apps/Settings...\e[0m";
-  cd packages/apps/Settings;
-  git pull $deadly$dcaf packages_apps_Settings --rebase;
-  croot;
-
-  echo " ";
-  echo -e "\e[1;32mRebasing build/make...\e[0m";
-  cd build/make;
-  git pull $deadly$dcaf build_make --rebase;
-  croot;
-
-  echo " ";
-  echo -e "\e[1;32mRebasing hardware/libhardware...\e[0m";
-  cd hardware/libhardware;
-  git pull $deadly$dcaf hardware_libhardware --rebase;
-  croot;
-
-  echo " ";
-  echo -e "\e[1;32mRebasing hardware/qcom/keymaster...\e[0m";
-  cd hardware/qcom/keymaster;
-  git pull $deadly$dcaf hardware_qcom_keymaster --rebase;
-  croot;
-
+  for d in ${dir2rebase[@]}; do
+    cd "$d";
+    echo " ";
+    echo -e "\e[1;32mRebasing "$d"...\e[0m";
+    d=$(echo $d | tr '[/]' '_');
+    git pull $deadly$dcaf "$d" --rebase;
+    croot;
+  done
 # ===================================================================================
